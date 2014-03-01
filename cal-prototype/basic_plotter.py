2cbdae067c85a431a35dc7bcde952ef744e6da37
Tobey Carman <tobeycarman@gmail.com>
2014-02-28 22:24:13 -0900

    some good modifications...

--- cal-prototype/basic_plotter.py
+++ cal-prototype/basic_plotter.py
@@ -13,16 +13,24 @@ def main():
 
   mt = os.stat('pass_thru.json').st_mtime
 
+  plotdata = {}
+
   while True:
     time.sleep(0.5)
     if os.stat('pass_thru.json').st_mtime == mt:
+      print "[PLOTTER] No change in pass thru file...nothing to do..."
+
       pass # nothing to do
     else:
       mt = os.stat('pass_thru.json').st_mtime
       with open('pass_thru.json') as infile:
         d = json.load(infile)
-      
-        print "[PLOTTER] add d to the local data and re-draw plot...", d
+        
+      print d['year'], d['month'], d['gpp']
+      plotdata[(d['year'], d['month'])] = d['gpp']
+  
+      print "[PLOTTER] added to the local data. new length: ", len(plotdata)
+
 
 if __name__ == '__main__':
   main()
