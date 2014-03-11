9e0bf6cdbedf452a854032a5fc0e27083c9b08dd
Tobey Carman <tobeycarman@gmail.com>
2014-03-11 09:43:11 -0800

    refactor so that each plot has its own file, each inheriting from the same base figure.

--- cal-prototype/plot_monthly_hydro.py
+++ cal-prototype/plot_monthly_hydro.py
@@ -12,53 +12,6 @@ from gpt.CalibrationFigure import CalibrationFigure
 
 from IPython import embed
 
-
-class MonthlyThermalFigure(CalibrationFigure):
-  ''' ???'''
-
-  def __init__(self, timerange=100):
-    self._fig, self._axes = plt.subplots(4,1,sharex='all')
-    self._fig.suptitle('Monthly Thermal')
-
-    self._timerange = timerange
-
-    # build an empty container
-    empty_series = np.empty(self._timerange)
-    empty_series.fill(np.nan)
-
-    self._traces = [
-      {
-       'jsontag': 'TempAir',
-       'data': empty_series.copy(),
-       'artist': self._axes[0].plot( [],[], label="air temp" ) 
-      },
-      {
-       'jsontag': 'TempOrganicLayer',
-       'data': empty_series.copy(),
-       'artist': self._axes[0].plot( [],[], label="org. layer temp" )
-      },
-
-      {
-       'jsontag': 'TempMineralLayer',
-       'data': empty_series.copy(),
-       'artist': self._axes[1].plot( [],[], label="mineral layer temp" )
-      },
-      {
-       'jsontag': 'PAR',
-       'data': empty_series.copy(),
-       'artist': self._axes[1].plot( [],[], label="PAR" )
-      },
-      {
-       'jsontag': 'ActiveLayerDepth',
-       'data': empty_series.copy(),
-       'artist': self._axes[2].plot( [],[], label="active layer depth" )
-      },
-    ]
-  
-  
-    self.GGGG()
-    
-
 class MonthlyHydroFigure(CalibrationFigure):
   
   def __init__(self, timerange=100):
@@ -111,29 +64,14 @@ class MonthlyHydroFigure(CalibrationFigure):
 
 def main():
 
-  T = 12 * 100  # monthly data for 100 years
+  mhf = MonthlyHydroFigure( timerange=T )  # <- MONTHS!
 
-#  emf = CalibrationFigure( timerange=10 )
-
-#  mhf = MonthlyHydroFigure( timerange=T )
-# 
-#   
-#   ani = animation.FuncAnimation( mhf.figure(), 
-#                                  mhf.update, 
-#                                  interval=100, 
-#                                  fargs=()
-#                                 )
-
-
-  mtf = MonthlyThermalFigure( timerange=T )
-
-  ani = animation.FuncAnimation( mtf.figure(), 
-                                 mtf.update, 
+  
+  ani = animation.FuncAnimation( mhf.figure(), 
+                                 mhf.update, 
                                  interval=100, 
                                  fargs=()
                                 )
-
-
 
   plt.show()
   
